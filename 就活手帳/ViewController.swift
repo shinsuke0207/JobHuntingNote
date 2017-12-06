bd60674a3ffdce533ab7f7dbc222178b5351eaf4
Shinsuke Katsumata <shinsuke@sern-2.local>
2017-12-06 22:53:59 +0900

    Initial Commit

---
+++ 就活手帳/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  就活手帳
+//
+//  Created by Shinsuke Katsumata on 2017/12/06.
+//  Copyright © 2017年 S.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
